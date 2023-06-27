c94c8b24e235ad8dda5bcf48ed58af8630642c28
k1mjunyoung <vovrwm@gmail.com>
2023-06-27 16:13:29 +0900

    3-5 스프링 시큐리티

---
+++ src/main/java/jumptospringboot/SecurityConfig.java
@@ -0,0 +1,30 @@
+package jumptospringboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+                .and()
+                // /h2-console/로 시작하는 URL은 CSRF 검증을 하지 않는다는 설정
+                .csrf()
+                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                ;
+        return http.build();
+    }
+}
