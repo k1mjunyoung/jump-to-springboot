f59ac901705275483900567fe9704bde2e483d79
k1mjunyoung <vovrwm@gmail.com>
2023-07-03 10:33:22 +0900

    Update JumpToSpringbootApplicationTests.java

--- src/test/java/jumptospringboot/JumpToSpringbootApplicationTests.java
+++ src/test/java/jumptospringboot/JumpToSpringbootApplicationTests.java
@@ -14,10 +14,9 @@ class JumpToSpringbootApplicationTests {
 	@Test
 	void testJPA() {
 		for (int i=1; i<=300; i++) {
-			String subject = String.format("테스트 데이터 입니다:[%03d]", i);
-			String content = "내용무";
-			this.questionService.create(subject, content);
+			String subject = String.format("테스트 데이터 입니다 [%03d]", i);
+			String content = String.format("내용무 [%03d]", i);
+			this.questionService.create(subject, content, null);
 		}
 	}
-
 }
